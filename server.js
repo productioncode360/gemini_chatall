165885ff92fd805fc40f0adf84c86d89f680c58e
Akhil80git <akhilgarg8085@gmail.com>
2026-04-01 22:48:21 +0530

    Your message here

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ app.use(cors({
     "http://127.0.0.1:5500",
     "http://127.0.0.1:5501",
     "http://localhost:5501",
+    "https://gemini-chatall.onrender.com",
+    "https://aimap-lovat.vercel.app/",
+
     "*"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
